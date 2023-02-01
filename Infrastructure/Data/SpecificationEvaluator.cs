3276b6f5112366ba7e57cc77b2ee8194f1e69122
Bryan King <goldbrickin@gmail.com>
2023-01-31 22:26:59 -0500

    End of section 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -16,7 +16,11 @@ namespace Infrastructure.Data
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
-
+            query = spec.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
+            //Usage Example
+            //AddInclude(c => c.CountryOfBirth);
+            //AddInclude(“CountyOfOrigin.DefaultCountryLanguage”);
+            
             return query;
         }
     }
